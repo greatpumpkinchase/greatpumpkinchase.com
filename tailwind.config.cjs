4c6e76f6a52763019f0c0749833cc946767b407c
Luke Bouch <luke@lukebouch.com>
2023-09-14 15:04:45 -0400

    Change color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                'beige': '#FFF3E3',
+                'beige': '#FFF4E6',
             },
         },
     },
