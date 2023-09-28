05685e7dcdd84c85089de42aaaf13fc810b99877
Luke Bouch <luke@lukebouch.com>
2023-09-28 15:45:18 -0400

    Add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,27 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://greatpumpkinchase.com",
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
+    site: "https://greatpumpkinchase.com",
+    adapter: netlify(),
+    integrations: [tailwind({
+        config: {
+            applyBaseStyles: false
+        }
+    }), sitemap(), alpinejs()],
+    redirects: {
+        '/register': {
+            status: 302,
+            destination: 'https://greatpumpkinchase.ticketbud.com/gpc'
+        },
+        '/volunteer/register': {
+            status: 302,
+            destination: 'https://docs.google.com/spreadsheets/d/1nCgrv27HLnYFlpsd1Qx9ZD0LcB86OyztRiAsmxept9w'
+        }
     }
-  }), sitemap(), alpinejs()],
-  redirects: {
-    '/register': {
-      status: 302,
-      destination: 'https://greatpumpkinchase.ticketbud.com/gpc'
-    },
-    '/volunteer/register': {
-      status: 302,
-      destination: 'https://docs.google.com/spreadsheets/d/1nCgrv27HLnYFlpsd1Qx9ZD0LcB86OyztRiAsmxept9w'
-    }
-  }
 });
