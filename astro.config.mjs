9e9413fb6dd3deaf63092e74713dd04636ffa2a4
Luke Bouch <luke@lukebouch.com>
2023-09-28 15:02:23 -0400

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,25 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://greatpumpkinchase.com",
-    integrations: [tailwind({
-        config: {
-            applyBaseStyles: false
-        }
-    }), sitemap()],
-    redirects: {
-        '/register': {
-            status: 302,
-            destination: 'https://greatpumpkinchase.ticketbud.com/gpc'
-        },
-        '/volunteer/register': {
-            status: 302,
-            destination: 'https://docs.google.com/spreadsheets/d/1nCgrv27HLnYFlpsd1Qx9ZD0LcB86OyztRiAsmxept9w'
-        }
+  site: "https://greatpumpkinchase.com",
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
     }
+  }), sitemap(), alpinejs()],
+  redirects: {
+    '/register': {
+      status: 302,
+      destination: 'https://greatpumpkinchase.ticketbud.com/gpc'
+    },
+    '/volunteer/register': {
+      status: 302,
+      destination: 'https://docs.google.com/spreadsheets/d/1nCgrv27HLnYFlpsd1Qx9ZD0LcB86OyztRiAsmxept9w'
+    }
+  }
 });
