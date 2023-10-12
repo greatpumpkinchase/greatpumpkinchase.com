c9046222324c3081a29dbb86bc707e0faff84b56
Luke Bouch <luke@lukebouch.com>
2023-10-12 17:03:29 -0400

    WIP

---
+++ src/components/TicketsAvailable.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p v-if="availableTickets !== undefined">Available Tickets: {{ availableTickets }}</p>
+        <p v-else>Loading...</p>
+    </div>
+</template>
+
+<script setup>
+    import { onMounted } from 'vue';
+
+    let availableTickets;
+
+    async function getTicketsAvailable() {
+        const response = await fetch('/api/getTicketsAvailable');
+        const data = await response.json();
+
+        return data;
+    }
+
+    onMounted(async () => {
+        availableTickets = await getTicketsAvailable();
+    });
+</script>
