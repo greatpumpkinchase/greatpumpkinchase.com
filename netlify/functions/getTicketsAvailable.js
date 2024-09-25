2cef4e649a5d6b6cec2e4692e7c4e01579d12767
Luke Bouch <luke@lukebouch.com>
2024-09-24 22:03:03 -0400

    Add more comments

--- netlify/functions/getTicketsAvailable.js
+++ netlify/functions/getTicketsAvailable.js
@@ -1,7 +1,11 @@
 const totalTickets = 100;
 
 exports.handler = async function (event, context) {
+    // Figure out a way to store the api token from NocoDB in a way that it's not stored in Git
+    // You can probably store it in some type of .env file
+
     // Call the NocoDB api
+    // The endpoint to call is https://app.nocodb.com/api/v2/tables/m6tawb2iaiuw50v/records/count
     let registrationCount = 0; // Set this to the number of rows in the "Student Registrations" table in NocoDB.
 
     let count = totalTickets - registrationCount; // Calculate the number of tickets left
