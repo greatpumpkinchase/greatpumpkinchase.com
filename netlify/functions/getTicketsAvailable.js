c06a3b9c3e74bc7886f66560adbdf8d80e0d1ede
Luke Bouch <luke@lukebouch.com>
2024-09-24 22:00:38 -0400

    Add comments for Zeo

--- netlify/functions/getTicketsAvailable.js
+++ netlify/functions/getTicketsAvailable.js
@@ -1,56 +1,14 @@
-import { Redis } from '@upstash/redis';
-
-const redis = new Redis({
-    url: 'https://us1-clever-poodle-40498.upstash.io',
-    token: 'AZ4yASQgYWU5YjQ2MTktZGQ1Ny00OGUyLWI2ODItODhkM2QwYTYxZWUyMDQzOTI0ZjJhNzc5NGViZmE4MzNlNmVlNDFjZjFjOTk=',
-});
+const totalTickets = 100;
 
 exports.handler = async function (event, context) {
-    let response = await getEventDetails();
+    // Call the NocoDB api
+    let registrationCount = 0; // Set this to the number of rows in the "Student Registrations" table in NocoDB.
 
-    if (response.ok !== true) {
-        data = await getNewRefreshToken();
-        await redis.set('refreshToken', data.refresh_token);
-        await redis.set('accessToken', data.access_token);
+    let count = totalTickets - registrationCount; // Calculate the number of tickets left
 
-        response = await getEventDetails();
-    }
-
-    data = await response.json();
-
+    // Return the count as JSON
     return {
         statusCode: 200,
-        body: JSON.stringify(data.event.tickets_available),
+        body: JSON.stringify(count),
     };
 };
-
-async function getEventDetails() {
-    let accessToken = await redis.get('accessToken');
-    let response = null;
-
-    response = await fetch(
-        "https://api.ticketbud.com/events/611668.json?access_token=" + accessToken,
-    );
-
-    return response;
-}
-
-async function getNewRefreshToken() {
-    const refreshToken = await redis.get('refreshToken');
-    response = await fetch("https://api.ticketbud.com/oauth/token", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            client_id: "x3LxS71Msxe9Om7MSR6biv5HI9R_fVekg91kJLtzvxM",
-            client_secret: "YdN0fyQTFi74BiGZHqvoUyS5ZJI_XpbxUvesTKX5JSY",
-            refresh_token: refreshToken,
-            grant_type: "refresh_token",
-            redirect_uri: "https://greatpumpkinchase.com",
-        }),
-    });
-
-    let data = await response.json();
-    return { "access_token": data.access_token, "refresh_token": data.refresh_token };
-}
