9818264e47e7804e97230a1bbd6c24c254cbab02
Luke Bouch <luke@lukebouch.com>
2025-09-02 23:27:06 -0400

    Open registration

--- src/js/isRegistrationOpen.js
+++ src/js/isRegistrationOpen.js
@@ -2,8 +2,8 @@ import moment from 'moment-timezone';
 
 export default function () {
     // Define the start and end dates in EST
-    const startDate = moment.tz('2024-10-25 17:30', 'America/New_York').tz('UTC', true);
-    const endDate = moment.tz('2024-10-25 18:30', 'America/New_York').tz('UTC', true);
+    const startDate = moment.tz('2025-09-02', 'America/New_York').tz('UTC', true);
+    const endDate = moment.tz('2025-10-23', 'America/New_York').tz('UTC', true);
 
     // Get the current time in UTC
     const now = moment.utc();
