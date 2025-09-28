356cc4274e0a041b5b5163319b7e458be81cea40
Luke Bouch <luke@lukebouch.com>
2025-09-28 19:39:21 -0400

    Update isRegistrationOpen.js

--- src/js/isRegistrationOpen.js
+++ src/js/isRegistrationOpen.js
@@ -3,7 +3,7 @@ import moment from 'moment-timezone';
 export default function () {
     // Define the start and end dates in EST
     const startDate = moment.tz('2025-09-02', 'America/New_York').tz('UTC', true);
-    const endDate = moment.tz('2025-10-23', 'America/New_York').tz('UTC', true);
+    const endDate = moment.tz('2025-10-18', 'America/New_York').tz('UTC', true);
 
     // Get the current time in UTC
     const now = moment.utc();
